26f0f494030b0508ad4b9d2c2879bca029386167
Lxk3-W <lward248@gmail.com>
2025-06-30 14:16:32 +0100

    can now read pdb file

--- open_and_read_pdb.py
+++ open_and_read_pdb.py
@@ -1,2 +1,8 @@
 import numpy
-import 
+import pickle
+
+file = 'C:\\Users\\lward\\Downloads\\8s6k.cif'
+with open(file, 'rb') as fp:
+    protein_stucture = fp.read()
+
+print(type(protein_stucture))
