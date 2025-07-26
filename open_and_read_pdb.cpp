eb904dcbf2c9b724d123c686592b033ccf4fd27a
L-uke-W <lward248@gmail.com>
2025-07-26 14:47:51 +0100

    updating code

--- open_and_read_pdb.cpp
+++ open_and_read_pdb.cpp
@@ -1,9 +1,14 @@
+/*
+    Parse the pdb file and stare the contents 
+*/
+
 #include<iostream>
 #include<fstream>
 using namespace std;
 
-char file = 'C:\\Users\\lward\Downloads\8s6k.cif';
 
 int main() {
+    
+
     
 }
