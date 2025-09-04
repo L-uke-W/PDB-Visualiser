7d81a0e0afec6b901c265535102bb5368b66ff33
L-uke-W <lward248@gmail.com>
2025-09-04 15:34:57 +0100

    another change

--- open_and_read_pdb.cpp
+++ open_and_read_pdb.cpp
@@ -2,8 +2,8 @@
     Parse the pdb file and stare the contents 
 */
 
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 using namespace std;
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
     char filepath[20]; //get the file
-    vector<string> file; // hold the file contents
+    string file; // hold the file contents
 
     
 }
