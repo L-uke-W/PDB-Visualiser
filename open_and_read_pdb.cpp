a4523b5ddc0af008b71d44592e62c0ae23cdc6e2
L-uke-W <lward248@gmail.com>
2025-09-04 15:33:26 +0100

    another update not much extra

--- open_and_read_pdb.cpp
+++ open_and_read_pdb.cpp
@@ -4,11 +4,14 @@
 
 #include<iostream>
 #include<fstream>
+#include <vector>
+#include <string>
 using namespace std;
 
 
 int main() {
-    
+    char filepath[20]; //get the file
+    vector<string> file; // hold the file contents
 
     
 }
